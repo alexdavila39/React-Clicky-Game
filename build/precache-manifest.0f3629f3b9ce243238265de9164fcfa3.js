26105cc100253cd6dab89caa06dc39bbc1b2a1be
Alejandro <alexdavila39@gmail.com>
2019-04-09 21:38:23 -0400

    Style in progress

---
+++ build/precache-manifest.0f3629f3b9ce243238265de9164fcfa3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "63768f5fdba625a7b9c7",
+    "url": "/React-Clicky-Game/static/css/main.7bfca0c2.chunk.css"
+  },
+  {
+    "revision": "63768f5fdba625a7b9c7",
+    "url": "/React-Clicky-Game/static/js/main.c62f0f45.chunk.js"
+  },
+  {
+    "revision": "339ef2b4a9d9b3bbcf8d",
+    "url": "/React-Clicky-Game/static/js/runtime~main.25e3a6ff.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/React-Clicky-Game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "11e0838e1308ac795fbd4fbf3290e7c9",
+    "url": "/React-Clicky-Game/index.html"
+  }
+];
